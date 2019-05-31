2a9874ccc46460561b4642ac4b2f507e5208545d
Carlossantana987 <carlossantana987@outlook.com>
2019-05-31 03:20:53 -0400

    finished todo project

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,19 +1,40 @@
 import React from "react";
 
-const TodoForm = (props) => {
-  return (
-    <form className="form" onSubmit={e => props.addTask(e)}>
-      <input
-      type="text" 
-      name="taskInput" 
-      placeholder='todo...'   
-      value={props.taskInput}
-      onChange={(e) => props.changeHandler(e)}
-      />
-      <button>Add</button>
-      <button>Clear Completed</button>
-    </form>
-  );
-};
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      item: " "
+    };
+  }
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  addItem = e => {
+    e.preventDefault();
+    this.props.addTask(e, this.state.item);
+    this.setState({
+      item: " "
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.addItem}>
+        <input
+          name="item"
+          placeholder="todo..."
+          value={this.state.item}
+          onChange={this.handleChanges}
+        />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
 
 export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,9 +3,14 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = (props) => {
-  return <div> {props.tasks.map((task) => <Todo task={task} />)} </div>
-
+const TodoList = props => {
+  return (
+    <div className="todo-list">
+      {props.tasks.map(task => {
+        return <Todo key={task.id} toggleTask={props.toggleTask} task={task} />;
+      })}
+    </div>
+  );
 };
 
 export default TodoList;
